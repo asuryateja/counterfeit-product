c5f5b42196522de2cfafc098648a7403d7632382
Surya Teja <88485967+asuryateja@users.noreply.github.com>
2023-04-02 08:55:00 +0530

    Update Authenticate.js

--- src/components/Authenticate.js
+++ src/components/Authenticate.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { QrReader } from "react-qr-reader";
+import React, { useState, useEffect , useRef} from "react";
+import QrScanner from "qr-scanner";
 import axios from "axios";
 import Lottie from "react-lottie";
 import animationData from "../lotties/4.json";
 
 import "../css/Authenticate.css";
+
 const Authenticate = ({ account }) => {
   const defaultOptions = {
     loop: true,
@@ -15,81 +16,109 @@ const Authenticate = ({ account }) => {
     },
   };
 
+  const handleClick = () => {
+    fileRef.current.click();
+    console.log(file);
+  };
+
+  const handleChange = async (e) => {
+    const file = e.target.files[0];
+    console.log(file);
+    setFile(file);
+
+    try {
+      await QrScanner.scanImage(file);
+    } catch (e) {
+      console.log(e); 
+      setVerify(false);
+      setMessage("Product is Not Authenticated ❌");
+    }
+
+    const result = await QrScanner.scanImage(file)
+    console.log(result);
+    try{ 
+    let data = JSON.parse(result);
+    
+    console.log(data.hash);
+    setHash(data.hash);
+    
+    let res = await axios.get(
+      `https://api-goerli.etherscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash=${hash}&apikey=${process.env.REACT_APP_ETHERSCAN_API_KEY}`
+    );
+    if (res) {
+      console.log(res);
+      setVerify(true);
+      setMessage("Product is Authenticated ✅");
+      setAuth(true);
+    }
+    }
+    catch(e){
+      console.log(e);
+      setVerify(false);
+      setMessage("Product is Not Authenticated ❌");
+    }
+
+  };
+
+  const handleVerify = async () => {
+    if (hash) {
+      let res = await axios.get(
+        `https://api-goerli.etherscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash=${hash}&apikey=${process.env.REACT_APP_ETHERSCAN_API_KEY}`
+      );
+      if (res) {
+        console.log(res);
+        setVerify(true);
+        setMessage("Product is Authenticated ✅");
+        setAuth(true);
+      }
+      else{
+        setVerify(false);
+        setMessage("Product is Not Authenticated ❌");
+      }
+    }
+  };
+
+  const [file, setFile] = useState(null);
+  const fileRef = useRef(null);
   const [auth, setAuth] = useState(false);
   const [message, setMessage] = useState("");
+  const [hash, setHash] = useState("");
+  const [verify, setVerify] = useState(null);
+
+  useEffect(() => {
+    if (verify !== null) {
+      handleVerify();
+    }
+  }, [verify]);
+
   return (
     <>
-      <div className="cam">
-        <h4 style={{fontSize: '20px' ,color: "#f7f7f7", position: "fixed", right: 8, top: 2 }}>
-          Wallet Address:{" "}
-          {account.substring(0, 4) +
-            "..." +
-            account.substring(account.length - 4, account.length)}
-        </h4>
-        <br />
-        <h2 style={{color: "#f7f7f7", position: "absolute", top: 20 }}>
-          Hold QR Code Steady and Clear to Scan
-        </h2>
-        <QrReader
-          onResult={async (result, error) => {
-            if (!!result && !!result?.text) {
-              let data = JSON.parse(result?.text);
-              if (data.hash) {
-                //HV1DID1M2FQ3Z9GKVSK6T2BKCNB8X89UCT
-                let res = await axios.get(
-                  `https://api-goerli.etherscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash=${data.hash}&apikey=${process.env.REACT_APP_ETHERSCAN_API_KEY}`
-                );
-                if (res) {
-                  console.log(res);
-                  <>
-                  <Lottie options={defaultOptions} height={300} width={300} />
-                  </>
-                  setMessage("Product is Authenticated ✅");
-                  setAuth(true);
-                }
-              }
-            }
-            if (!!error) {
-              //setMessage("Product is Not Authenticated ❌");
-              console.info(error);
-            }
-          }}
-          style={{ width: "100%" }}
-        />
-        <div
-          style={{
-            position: "absolute",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            top: "50%",
-            color: "#f7f7f7"
-          }}
-        >
-          <div>
-            <h1 style={{color: "#f7f7f7"}}>{message}
-            {
-              (
-                ()=>{
-                  if(auth){
-                    return(
-                    <Lottie options={defaultOptions} height={300} width={300} />)
-                  }
-                }
-              )()
-            }
-            </h1>
-          </div>
+      <center>
+        <div className='container'>
+          <button
+            type="button"
+            onClick={handleClick}
+            className="btn btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+            style={{ backgroundColor: "#A9A9A9", color: "#28282B", border: "none", margin: "20px", padding: "10px 20px" , borderRadius: "10px", fontWeight: "bold", marginTop: "10%"}}
+          >
+            Scan QR Code
+          </button>
+          <input
+            type="file"
+            ref={fileRef}
+            onChange={handleChange}
+            accept=".png, .jpg, .jpeg"
+            style={{ display: "none" }}
+          />
         </div>
-        <div style={{color: "#f7f7f7", position: "absolute", bottom: 90 }}>
-          <h3>
-            Please wait for 15 sec if Authentication messages is not appearing
-            on the screen then your product is not Authenticated.
-          </h3>
-          <br />
-          <span>Please reload the page to Scan again.</span>
-        </div>
+      </center>
+      <div>
+        <h1 style={{ color: "#E5E4E2", textAlign: "center", marginTop: '20px' }}>
+          {message}
+          {verify && <Lottie options={defaultOptions} height={300} width={300} />}
+        </h1>
       </div>
     </>
   );
